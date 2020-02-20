54c22dfaf1f595d6279e12a5c38b1f925a146019
Christian <christian@mail.de>
2020-02-20 18:49:33 +0100

    challenge plus resources added

---
+++ src/main/java/q2020/MainTest.java
@@ -0,0 +1,7 @@
+package q2020;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+}
--- src/main/java/q2020/model/Problem.java
+++ src/main/java/q2020/model/Problem.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Map;
 
 public class Problem {
+
 	private final int slideCount;
 
 	public Map<String, Integer> getTagIds() {
